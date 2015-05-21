71f590e4cf4849a1d4b32d8541dd6e7ef048ebe5
Sergi Martínez <sergi.martinez@gmail.com>
2015-05-21 17:30:36 +0200

    The right bitmap loaders

--- app/src/main/java/com/sergiandreplace/canvas/utils/BitmapUtils.java
+++ app/src/main/java/com/sergiandreplace/canvas/utils/BitmapUtils.java
@@ -25,15 +25,58 @@ import android.graphics.BitmapFactory;
  */
 public class BitmapUtils {
     public static Bitmap loadBitmap(Context context, int resource) {
-        Bitmap b= BitmapFactory.decodeResource(context.getResources(), resource);
+        Bitmap b = BitmapFactory.decodeResource(context.getResources(), resource);
         return b;
     }
 
     public static Bitmap loadMutableBitmap(Context context, int resources) {
-        Bitmap b=loadBitmap(context,resources);
-        Bitmap mb=b.copy(Bitmap.Config.ARGB_8888,true);
+        Bitmap b = loadBitmap(context, resources);
+        Bitmap mb = b.copy(Bitmap.Config.ARGB_8888, true);
         b.recycle();
         return mb;
 
+    }
+
+    public static Bitmap loadResizedBitmap(Context context, int resource, int width, int height) {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(context.getResources(), resource, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, width, height);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(context.getResources(), resource, options);
+    }
+
+        public static Bitmap loadMutableResizedBitmap(Context context, int resource, int width, int height) {
+            Bitmap b = loadResizedBitmap(context, resource, width,height);
+            Bitmap mb = b.copy(Bitmap.Config.ARGB_8888, true);
+            b.recycle();
+            return mb;
+        }
+
+        public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 }
